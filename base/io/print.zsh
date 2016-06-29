18b378586cab65cec8eef17883f4a4562cfce01f
b4b4r07 <b4b4r07@gmail.com>
2016-06-29 12:54:48 +0900

    __zplug::io::print::f

--- base/io/print.zsh
+++ base/io/print.zsh
@@ -7,3 +7,39 @@ __zplug::io::print::die()
 {
     command printf -- "$@" >&2
 }
+
+__zplug::io::print::f()
+{
+    local -a formats texts
+    local    arg text
+
+    while (( $# > 0 ))
+    do
+        arg="$1"
+        case "$arg" in
+            --zplug)
+                formats+=( "[zplug] " )
+                ;;
+            --warn)
+                formats+=( "$fg[red]${(%):-"%U"}WARNING${(%):-"%u"}$reset_color: " )
+                ;;
+            --error)
+                formats+=( "$fg[red]ERROR$reset_color: " )
+                ;;
+            --)
+                texts+=( "\n" )
+                ;;
+            *)
+                texts+=( "$arg" )
+                ;;
+        esac
+        shift
+    done
+
+    texts=( "$formats[@]" "$texts[@]" )
+
+    for text in "$texts[@]"
+    do
+        command printf -- "$text"
+    done
+}
