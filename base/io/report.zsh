c492c01d8a7bfd7ecc0fb16b09b7c7a40e2bff48
b4b4r07 <b4b4r07@gmail.com>
2016-07-23 18:08:33 +0900

    json

--- base/io/report.zsh
+++ base/io/report.zsh
@@ -5,20 +5,43 @@ __zplug::io::report::write()
 
 __zplug::io::report::reporter()
 {
-    local -a header data
+    local -a header results
+    local    w=""
+    local -i i=0
 
     # Assume the stdin that should be discarded to /dev/null
-    data=( ${(@f)"$(<&0)"} )
+    results=( ${(@f)"$(<&0)"} )
 
     header=()
     header+=( "[$(date +"%Y/%m/%d %T")]" )
-    if [[ -n $funcstack[-1] ]]; then
-        header+=( "$funcstack[-1]" )
-    fi
-    header+=( "(${PWD/$HOME/~})" )
+    header+=( "${PWD/$HOME/~}" )
 
-    # Log report
-    print -l "${header[*]}: ${^data[@]}"
+    # Make spaces
+    for ((; i < ${#${header[*]}}; i++))
+    do
+        w="$w "
+    done
+
+    # Spit out to JSON
+    printf '{'
+    printf '"date": "%s",' "$(date +"%Y/%m/%d %T")"
+    printf '"directory": "%s",' "$PWD"
+    printf '"result": "%s",' "${results[*]}"
+    printf '"trace": {'
+    for ((i = 1; i < $#functrace; i++))
+    do
+        # Comma
+        printf "\"%s\": \"%s\"," \
+            "$functrace[$i]" \
+            "$funcstack[$i]"
+    done
+    printf "\"%s\": \"%s\"" "$functrace[$#functrace]" "$funcstack[$#funcstack]"
+    printf "}}\n"
+
+    # $funcfiletrace[@]
+    # $funcsourcetrace[@]
+    # $funcstack[@]
+    # $functrace[@]
 }
 
 __zplug::io::report::save()
